f6be4b0ff1a9c7db3975c876e57b5327d5b13403
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-28 18:00:07 -0800

    Setup passport

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var express = require('express');
-var mongoose = require('mongoose');
+var express                 = require('express'),
+    mongoose                = require('mongoose'),
+    passport                = require('passport'),
+    bodyParser              = require('body-parser'),
+    LocalStrategy           = require('passport-local'),
+    passportLocalMongoose   = require('passport-local-mongoose'),
+    User                    = require('./models/user');
+    
 mongoose.connect('mongodb://localhost/auth_demo_app');
 var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require('express-session')({
+    secret: "Appa is the best",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res){
     res.render('home');
